bb05b033128ea7dfdf2ad420614fa8a53c85983e
mt69JMMW <160133602+mt69JMMW@users.noreply.github.com>
2024-03-07 15:52:23 +0800

    Update finetune.sh

--- scripts/finetune.sh
+++ scripts/finetune.sh
@@ -3,16 +3,13 @@ CUDAs=(${TOT_CUDA//,/ })
 CUDA_NUM=${#CUDAs[@]}
 PORT="12345"
 
-DATA_PATH="../data/scimrc_for_inst_tune_sec_based.json"
-OUTPUT_PATH="Vicuna-LoRA"
-MODEL_PATH="../llm_weights/vicuna-7b-v1.3"
-TEST_SIZE=800
+DATA_PATH="/path/to/your/data"
+OUTPUT_PATH="/path/to/saved/lora/weights"
+MODEL_PATH="/path/to/model"
 
 CUDA_VISIBLE_DEVICES=${TOT_CUDA} torchrun --nproc_per_node=$CUDA_NUM --master_port=$PORT finetune.py \
 --data_path $DATA_PATH \
 --output_path $OUTPUT_PATH \
 --model_path $MODEL_PATH \
 --eval_steps 200 \
---save_steps 200 \
---test_size $TEST_SIZE
-# > logs/peft_vicuna_20230731.log 2>&1 &
+--save_steps 200
