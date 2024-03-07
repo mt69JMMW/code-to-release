1de177dfeb595b7aa09e39fb4fa1c92817ed3242
mt69JMMW <160133602+mt69JMMW@users.noreply.github.com>
2024-03-07 14:20:01 +0800

    Add files via upload

--- scripts/metric.sh
+++ scripts/metric.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-MODEL_NAME="/mnt/data/zekai/flan-t5-large"
-DATA_PATH="/mnt/data/zekai/summaries_after_tune_1200.json"
-SAVE_PATH="/mnt/data/zekai/qa_metric_result_1200.json"
+MODEL_PATH="/mnt/data/zekai/flan-t5-large-squad2"
+DATA_PATH="/mnt/data/zekai/generator_data_qasper.json"
+PREDICTION_PATH="/mnt/data/zekai/iter_refine_qasper_flan_t5_900.json"
+SAVE_PATH="iter_refine_qasper_flan_t5_qags_scores.json"
+QG_MODEL_PATH="lmqg/t5-large-squad-qg"
 
-nohup python3 metric.py \
---model_name $MODEL_NAME \
+nohup python3 metrics.py \
+--model_path $MODEL_PATH \
 --data_path $DATA_PATH \
+--prediction_path $PREDICTION_PATH \
 --save_path $SAVE_PATH \
---metrics_name "rouge" \
-> logs/qa_metric_rouge_20230902_1200.log 2>&1 &
+--max_new_tokens 100 \
+--num_beams 2 \
+--n_questions 20 \
+--qg_model_path $QG_MODEL_PATH \
+--from_refine "True" \
+> logs/metrics_iter_refine_qasper_flan_t5_20230201.log 2>&1 &
